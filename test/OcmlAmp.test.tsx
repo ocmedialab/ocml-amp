43191358d19791c41a9bb7f84dc2601a9e794a3c
Marcus Badillo <marcusb003@hotmail.com>
2022-11-18 05:24:50 -0600

    tes

--- test/OcmlAmp.test.tsx
+++ test/OcmlAmp.test.tsx
@@ -2,12 +2,15 @@ import * as React from 'react';
 import { render } from '@testing-library/react';
 import { OcmlAmp } from '../src/';
 
+// In your test setup file
+// globalThis.IS_REACT_ACT_ENVIRONMENT = true;
+
 describe('it', () => {
   it('renders default props', () => {
     render(<OcmlAmp />);
   });
 
   it('renders with manual', () => {
-    render(<OcmlAmp withManual />);
+    render(<OcmlAmp userManual />);
   });
 });
